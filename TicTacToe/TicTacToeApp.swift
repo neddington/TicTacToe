e22fbcac9b6bff4870f4bc22422c0a8d04937d54
Nick Eddington <neddington@cascadia.edu>
2023-05-30 12:31:08 -0700

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Eddington, Nick on 5/30/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
