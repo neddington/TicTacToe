4462d774c56b2e5b04ec728f2163e3b50c12d58e
Nick Eddington <neddington@cascadia.edu>
2023-05-30 12:36:25 -0700

    Step 1

---
+++ TicTacToe/Models/TicModel.swift
@@ -0,0 +1,12 @@
+//
+//  TicModel.swift
+//  TicTacToe
+//
+//  Created by Eddington, Nick on 5/30/23.
+//
+
+import Foundation
+
+struct TicModel {
+
+}
---
+++ TicTacToeTests/ModelTests/TicModelTests.swift
@@ -0,0 +1,22 @@
+//
+//  TicModelTests.swift
+//  TicTacToeTests
+//
+//  Created by Eddington, Nick on 5/30/23.
+//
+
+import XCTest
+@testable import TicTacToe
+
+class TicModelTests: XCTestCase {
+
+    func test_initialValue_NineCells() {
+        // Arrange
+        let ticModel = TicModel()
+        
+        // Act
+        
+        // Assert
+    }
+
+}
